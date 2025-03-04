5fc461abb37ac0910b05df864b42f3a11b8f715c
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-03-04 09:47:21 -0700

    choosing NR

--- src/goph420_lab03/open_methods.py
+++ src/goph420_lab03/open_methods.py
@@ -1,3 +1,29 @@
 # for step 4 part 1
-# "Write Python function(s) that find the root of a given function using either the Newton-Raphson Method or the
-# Modified Secant Method"
+# "Write Python function(s) that find the root of a given function using the Newton-Raphson Method
+
+import numpy as np
+
+def root_newton_raphson(x0, f, dfdx):
+    """
+    Find the root of a given function using the Newton-Raphson Method.
+
+    Parameters:
+    -----
+    x0: initial guess.
+    f: the function to find the root of.
+    dfdx: derivative of the function, where dx is the step size.
+
+    Returns:
+    -----
+    final_est: float.
+        Final root estimate.
+    iter: int.
+        Iterations to convergence.
+    rel_error: numpy.ndarray
+        1D vector of the approx. relative error at each iteration.
+    """
+    x = x0
+    while True:
+        x = x - f(x) / dfdx(x)
+        if np.abs(f(x)) < 1e-10:
+            return x
