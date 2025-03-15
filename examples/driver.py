f01692431b947afadceb158bd71a98809c5558a4
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-03-15 15:13:33 -0600

    modifying graph code

--- examples/driver.py
+++ examples/driver.py
@@ -7,49 +7,12 @@ from src.goph420_lab03.open_methods import (
 
 
 # creating function to make the plot using part 2
-def dispersion(x, p_1, p_2, sqrt_f):
-    """
-    Uses equation 1.
-    """
-    f_zeta_max = (p_1 / p_2) * (np.sqrt(sqrt_f)/x) - np.tan(2*np.pi*x)  # equation 1
-    return f_zeta_max
-
-
-def dispersion_deriv(x, b_1, b_2, p_1, p_2, H, sqrt_f):
-    """
-    Derived equation 1.
-    """
-    dF_dzeta_max = ((p_2 / p_1) * (np.sqrt(sqrt_f) / x ** 2) -
-                   ((p_2/p_1) * (1/np.sqrt(H ** 2 * (b_1 ** (-2) - b_2 ** (-2))))) -
-                   (2*np.pi * (1 / np.cos(2*np.pi*x)**2)))
-    return dF_dzeta_max
-
-
-"""def zeta_func(b_1, b_2, p_1, p_2, H):
-
-    Using the Newton_Raphson root-finding method to solve for zeta.
-    **** might not use
-    
-    zeta_max = H * np.sqrt(b_1 ** (-2) + b_2 ** (-2))   # equation 2, zeta in terms of love wave velocity
-    sqrt_f = H * np.sqrt((b_1 ** -2) - (b_2 ** -2))
-
-    # checking zeta_max range!
-    if zeta_max < 0 or np.isnan(zeta_max):
-        return None
-
-    initial_guess = np.linspace(0.1, zeta_max, 10)
-    zeta_list = []  # for the modes
-
-    for x0 in initial_guess:
-        # note that 'lambda x' is used to pass the necessary arguments to the functions (so ignores x)
-        # actually solving for roots of dispersion
-        zeta = root_newton_raphson(x0,
-                               lambda x: dispersion(x, p_1, p_2, sqrt_f),
-                               lambda x: dispersion_deriv(x, b_1, b_2, p_1, p_2, H, sqrt_f))
-        if zeta is not None and zeta >= 0:
-            zeta_list.append(zeta)  # only keeping positive roots
-    # returning a list of valid zeta values
-    return zeta_list if zeta_list else None"""
+#def dispersion_deriv(x, b_1, b_2, p_1, p_2, H, sqrt_f):
+# derived equ
+    #dF_dzeta_max = ((p_2 / p_1) * (np.sqrt(sqrt_f) / x ** 2) -
+                   #((p_2/p_1) * (1/np.sqrt(H ** 2 * (b_1 ** (-2) - b_2 ** (-2))))) -
+                   #(2*np.pi * (1 / np.cos(2*np.pi*x)**2)))
+    #return dF_dzeta_max
 
 
 def main():
@@ -62,46 +25,49 @@ def main():
     # thickness is in units of m
     H = 4000
 
-    zeta_max = H * np.sqrt(b_1 ** (-2) + b_2 ** (-2))  # equation 2, zeta in terms of love wave velocity
-    sqrt_f = H * np.sqrt((b_1 ** -2) - (b_2 ** -2))
-
     # frequency is in Hz
-    freq = np.linspace(0.1, 2, 4)
+    freq = [0.1,0.5,1,1.5,2]
     nfreq = len(freq)
 
-    plt.figure(figsize=(10, 8))
+    fig, axes = plt.subplots(nrows=nfreq, sharex=True, figsize=(12, 12))
 
-    for j, f in enumerate(freq):
+    for j, (ax, f) in enumerate(zip(axes, freq)):
+        zeta_max = H * np.sqrt(b_1 ** (-2) + b_2 ** (-2))  # equation 2, zeta in terms of love wave velocity
+        sqrt_f = H * np.sqrt((b_1 ** -2) - (b_2 ** -2))
+
+        def dispersion(x):
+            return (p_1 / p_2) * (np.sqrt(sqrt_f) / x) - np.tan(2 * np.pi * x)  # equation 1
+
         asyms = [0.0]
-        t = 0
-        k = 0
+        a, k = 0, 0
 
-        while t < zeta_max:
-            t = 0.25 * (2 * k + 1) / f
-            if t < zeta_max:
-                asyms.append(t)
+        while a <= zeta_max:
+            a = 0.25 * (2 * k + 1) / f
+            if a < zeta_max:
+                asyms.append(a)
             k += 1
         asyms.append(zeta_max)
+        asyms = sorted(set(asyms))
+        asyms = [float(a) for a in asyms]
+        print(f'Asyms: {asyms}')
         n = len(asyms)
 
-        plt.subplot(nfreq, 1, j + 1)  # creating subplots
-
-        for k, tote in enumerate(asyms):  # looping over asyms list
-            if k and k < n - 1:
-                plt.plot([tote, tote], [-5, 5], "--r")  # plotting a red dash line at each asym
-            if k < n - 1:
-                zetaval = np.linspace(tote + 1e-3, asyms[k + 1] - 1e-3)  # creating zeta array between two asyms
-                print(f'Zeta values: {zetaval}')
-                disperse = dispersion(zetaval, p_1, p_2, H)
-                print(f'Dispersion values: {disperse}')
-                plt.plot(zetaval, disperse, "-b", label='Dispersion Values vs. Zeta')  # x,y
-            plt.grid()
-            plt.xlabel("Love Wave Velocity")
-            plt.ylabel("Dispersion")
-            plt.xlim((0.0, zeta_max))
-            plt.ylim((-5.0, 5.0))
-    plt.title('Dispersion vs Love Wave Velocity')
-    plt.legend()
+        #plt.subplot(nfreq, 1, j + 1)  # creating subplots
+        for k, ak in enumerate(asyms[:-1]):
+            if k > 0:
+                ax.plot([ak, ak], [-5, 5], '--b', label='Asymptotes')
+            if asyms[k + 1] - ak > 1e-3:
+                points = np.linspace(ak + 1e-3, asyms[k + 1] - 1e-3, num=100)
+                func = dispersion(points)
+                ax.plot(points, func, '-r', label='Love Wave Velocity')
+        ax.grid()
+        ax.set_xlabel("Love Wave Velocity")
+        ax.set_ylabel("Dispersion")
+        ax.set_xlim((0.0, zeta_max))
+        ax.set_ylim((-5.0, 5.0))
+        ax.set_title(f'Dispersion vs Love Wave Velocity (f = {f} Hz)')
+        ax.legend()
+    plt.subplots_adjust(hspace=0.4)  # Prevents overlap
     plt.savefig("C:/Users/sydne/git/goph420/goph420-w2025-lab03-stDFSP/figures/disperse_love_wave.png")
     plt.show()
 
